ea3e54559315fa13e098b74af5efbd1e667a943d
GFigs <sgabrielfigueroa95@gmail.com>
2025-04-04 21:00:08 +0200

    style: apply RuboCop auto-corrections

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,23 +1,23 @@
 class Product < ApplicationRecord
-    #standard validations
+    # standard validations
     validates :code, presence: true, uniqueness: true
     validates :name, presence: true
     validates :price, presence: true
 
-    #price format validation
+    # price format validation
     validate :price_format
 
     private
 
-    def price_format 
+    def price_format
         return if price.nil?
 
-        if price.to_d.to_s('F') =~ /\.\d{3,}/
-            errors.add(:price, 'must have at most 2 decimal places')
+        if price.to_d.to_s("F") =~ /\.\d{3,}/
+            errors.add(:price, "must have at most 2 decimal places")
         end
 
         if price >= 1_000_000
-            errors.add(:price, 'must be less than 1,000,000')
+            errors.add(:price, "must be less than 1,000,000")
         end
     end
 end
