1f7149878a2521ff68a246daf671d625a7d4871b
GFigs <sgabrielfigueroa95@gmail.com>
2025-04-05 17:47:04 +0200

    style: fix linting errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-    before_action :set_product, only: [:show, :update, :destroy]
-  
+    before_action :set_product, only: [ :show, :update, :destroy ]
+
     def index
       if params[:search].present?
         query = params[:search].downcase
@@ -8,24 +8,24 @@ class ProductsController < ApplicationController
       else
         @products = Product.all
       end
-  
+
       render json: @products
     end
-  
+
     def show
       render json: @product
     end
-  
+
     def create
       @product = Product.new(product_params)
-  
+
       if @product.save
         render json: @product, status: :created
       else
         render json: @product.errors, status: :unprocessable_entity
       end
     end
-  
+
     def update
       if @product.update(product_params)
         render json: @product
@@ -33,20 +33,20 @@ class ProductsController < ApplicationController
         render json: @product.errors, status: :unprocessable_entity
       end
     end
-  
+
     def destroy
       @product.destroy
       head :no_content
     end
-  
+
     private
-  
+
     def set_product
       @product = Product.find_by(id: params[:id])
       render json: { error: "Product not found" }, status: :not_found unless @product
     end
-  
+
     def product_params
       params.require(:product).permit(:name, :code, :price)
     end
-  end
+end
