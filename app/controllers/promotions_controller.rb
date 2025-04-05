0b12c1af55f7af3ff82889713851bf1809faab38
GFigs <sgabrielfigueroa95@gmail.com>
2025-04-05 21:24:50 +0200

    add promotions controller and CRUD endpoints with search support

---
+++ app/controllers/promotions_controller.rb
@@ -0,0 +1,59 @@
+class PromotionsController < ApplicationController
+    before_action :set_promotion, only: [:show, :update, :destroy]
+  
+    def index
+        if params[:name].present?
+          query = params[:name].downcase
+          @promotions = Promotion.where("LOWER(name) LIKE ?", "%#{query}%")
+        else
+          @promotions = Promotion.all
+        end
+      
+        render json: @promotions
+    end
+  
+    def show
+      render json: @promotion
+    end
+  
+    def create
+      @promotion = Promotion.new(promotion_params)
+  
+      if @promotion.save
+        render json: @promotion, status: :created
+      else
+        render json: @promotion.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @promotion.update(promotion_params)
+        render json: @promotion
+      else
+        render json: @promotion.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @promotion.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_promotion
+      @promotion = Promotion.find_by(id: params[:id])
+      render json: { error: "Not found" }, status: :not_found unless @promotion
+    end
+  
+    def promotion_params
+      params.require(:promotion).permit(
+        :name,
+        :promotion_type,
+        :product_id,
+        :new_price,
+        :discount_percentage,
+        :trigger_quantity
+      )
+    end
+  end
